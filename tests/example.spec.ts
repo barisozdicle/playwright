d77ce6401ef55e15331ad57b3634422515e037c2
barisozdicle <bo88@hotmail.co.uk>
2024-03-31 12:23:31 +0200

    Initial Steps

---
+++ tests/example.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', {tag: ['@baris', '@gizem']}, async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link',{
+  annotation:{
+    type:'issue',
+    description:'https://fareharbor.com/'
+  },
+}, async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
